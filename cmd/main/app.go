255f1d638937267da493272ee2cf69eaecb331b6
Ron Liu <ronliu@Rons-MBP-2.lan>
2018-12-28 21:13:45 +1100

    further refactory

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -23,24 +23,12 @@ type App struct {
 // NewApp is the constructor for App
 func NewApp(rep repo.Get, wsServer publisher.WebSocketServer, originAllowed string) *App {
 	a := App{rep, wsServer, mux.NewRouter(), originAllowed}
+	a.setupCors()
 	a.initializeRoutes()
 	return &a
 }
 
 func (a *App) initializeRoutes() {
-	// Handle all preflight request
-	a.Router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
-		w.WriteHeader(http.StatusNoContent)
-		return
-	})
-	a.Router.StrictSlash(true)
-	_ = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
-	_ = handlers.AllowedOrigins([]string{a.originAllowed})
-	_ = handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-
 	a.Router.HandleFunc("/home", a.GetHome).Methods("GET")
 	a.Router.HandleFunc("/ws", a.wsServer.ServeWebsocket)
 	a.Router.Path("/tx-blocks/{number:[0-9]+}").Queries("pagesize", "{pagesize}").HandlerFunc(a.GetBlocks)
@@ -118,24 +106,17 @@ func (a *App) Run() {
 	log.Fatal(http.ListenAndServe(":8000", a.Router))
 }
 
-type setupRoute func(*mux.Router)
-
-// CreateRouter is to generate the router
-func CreateRouter(f setupRoute, originAllowed string) {
-	r := mux.NewRouter()
+func (a *App) setupCors() {
 	// Handle all preflight request
-	r.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Printf("OPTIONS")
+	a.Router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	})
-	r.StrictSlash(true)
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
-	originsOk := handlers.AllowedOrigins([]string{originAllowed})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-	f(r)
-	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(methodsOk, headersOk, originsOk)(r)))
+	a.Router.StrictSlash(true)
+	_ = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	_ = handlers.AllowedOrigins([]string{a.originAllowed})
+	_ = handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 }
