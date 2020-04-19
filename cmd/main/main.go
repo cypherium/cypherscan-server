4de361b0882197bda1ee8b36e9e2705a20575503
JaxonZeng <182886016@qq.com>
2020-04-19 21:02:48 +0800

    cypherdb

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -20,7 +20,8 @@ func main() {
 	context := context.Background()
 	config := config.GetFromEnv()
 	log.Info("Config:", fmt.Sprintf("%v", config))
-	dbClient, err := util.ConnectDb("postgres", config.RdsHostName, config.RdsPort, config.RdsDbName, config.RdsUserName, config.RdsPassword, config.RdsSslMode)
+	if err != nil {
+	dbClient, err := util.ConnectDb("postgres", config.RdsHostName, config.RdsPort, "cypherdb", "postgres", "postgres", "disable")
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Can NOT connect to database: %s", err.Error()))
 	}
