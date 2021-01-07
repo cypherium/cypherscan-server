1eac380436033b740c0e8be758e9ff90f493d9db
japson1987 <182886016@qq.com>
2021-01-07 10:53:49 +0800

    add rm -rf out*.log when running start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cp -rf server.service  /etc/init.d/
 chmod 700 /etc/init.d/server.service
-
+rm -rf out*.log
 systemctl daemon-reload
 systemctl enable /etc/init.d/server.service
 systemctl start server.service
